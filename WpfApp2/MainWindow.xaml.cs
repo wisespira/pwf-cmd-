a05d38b505bed5beab04434da17ea580f8e1015d
niw22 <ira.watt@newcastle.ac.uk>
2019-05-30 17:01:38 +0100

    First commit

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string strCmdText;
+            strCmdText = "/C pause";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Process myProcess = new Process();
+
+            try
+            {
+                myProcess.StartInfo.UseShellExecute = false;
+                // You can start any process, HelloWorld is a do-nothing example.
+                myProcess.StartInfo.FileName = "H:\\Desktop\\bob.exe";
+                //myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.Start();
+
+                
+
+                // This code assumes the process you are starting will terminate itself. 
+                // Given that is is started without a window so you cannot terminate it 
+                // on the desktop, it must terminate itself or you can do it programmatically
+                // from this application using the Kill method.
+            }
+            catch (Exception entry)
+            {
+                Console.WriteLine(entry.Message);
+                Task.Run(() => { });
+            }
+        }
+    }
+}
